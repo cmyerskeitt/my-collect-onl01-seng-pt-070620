84508c8f925200702d3310124e18543be0a5c679
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-15 20:06:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,15 @@
+
+
 def my_collect(array)
  i=0 
  collection = []
  while i < array.length 
-  collection << yield(array[i])
-  i += 1
+    collection << yield(array[i])
+    i += 1
+  end
+end
  
+ my_collect()
  
  
  
